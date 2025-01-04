533651c6e58c4ebf6deb5faedffdb9c17e8ace0a
Murali Krishna <94690714+MuraliAC@users.noreply.github.com>
2025-01-04 22:52:33 +0530

    Delete New ceg directory

--- New ceg/script.js
+++
@@ -1,9 +0,0 @@
-// function adjustIframeHeight() {
-//     var iframe = document.getElementById('myIframe');
-//     console.log(iframe.contentWindow)
-//     // iframe.contentDocument.location.reload(true);
-//     iframe.style.height = +iframe.contentWindow.document.body.scrollHeight + 10 + 'px';
-//     console.log(iframe.contentWindow)
-// }
-
-iFrameResize({ log: true }, '#myIframe');
